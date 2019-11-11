db3ba86b33e89d04d37636a104f33b2cce4040b6
Alex Sayers <alex@asayers.com>
2019-11-11 18:15:57 +0900

    diff-benches: Summarize incoming records

--- diff-benches/src/main.rs
+++ diff-benches/src/main.rs
@@ -1,13 +1,53 @@
-fn main() {
-    println!("Hello, world!");
+use rolling_stats::Stats;
+use std::process::Command;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Options {
+    #[structopt(short, long)]
+    csv_out: bool,
+    benches: Vec<String>,
 }
 
-fn run(commit_a: Commit, commit_b: Commit) -> ConfidenceInterval {
-    let all_measurements = Set::<Measurement>::new();
+fn main() {
+    let opts = Options::from_args();
+    diff(&opts.benches);
+}
+
+fn diff(benches: &[String]) -> ConfidenceInterval {
+    let mut all_measurements = Vec::<(usize, f64)>::new();
     loop {
-        let measurement = bench(if rand().is_even() { commit_a } else { commit_b });
-        all_measurements.insert(measurement);
-        let ci = confidence_interval(all_measurements);
-        if ci.width() < THRESHOLD { return ci; }
+        let idx = rand::random::<usize>() % benches.len();
+        all_measurements.push((idx, run_bench(&benches[idx])));
+        let ci = confidence_interval(&all_measurements);
+        if ci.end - ci.start < THRESHOLD {
+            return ci;
+        }
     }
+}
+
+fn run_bench(cmd: &str) -> f64 {
+    let out = Command::new("/bin/sh")
+        .arg("-c")
+        .arg(cmd)
+        .output()
+        .unwrap()
+        .stdout;
+    serde_json::from_slice(&out).unwrap()
+}
+
+type ConfidenceInterval = std::ops::Range<f64>;
+
+const THRESHOLD: f64 = 1.0;
+const SIG_LEVEL: f64 = 1.0;
+
+fn summarize(xs: &[(usize, f64)]) -> Vec<Stats<f64>> {
+    let mut stats = vec![];
+    for (i, x) in xs {
+        if *i >= stats.len() {
+            stats.resize_with(i + 1, Stats::new)
+        }
+        stats[*i].update(*x);
+    }
+    stats
 }
