12001c827784061ff38d29a70d7ebdd91597d387
Alex Sayers <alex@asayers.com>
2019-11-11 12:54:57 +0900

    perf-bench: Add max RSS

--- perf-bench.sh
+++ perf-bench.sh
@@ -1,15 +1,20 @@
 #!/bin/bash -eu
 
-RESULTS=$(mktemp)
-perf stat --field-separator="," --output=$RESULTS -- $@ >/dev/null
-
-TASK_CLOCK=$(     grep ',task-clock,'    <$RESULTS | xsv select 1)
-CLOCK_UNITS=$(    grep ',task-clock,'    <$RESULTS | xsv select 2)
-CPU_UTILIZATION=$(grep ',task-clock,'    <$RESULTS | xsv select 6)
-INSTRUCTIONS=$(   grep ',instructions,'  <$RESULTS | xsv select 1)
-CYCLES=$(         grep ',cycles,'        <$RESULTS | xsv select 1)
-BRANCHES=$(       grep ',branches,'      <$RESULTS | xsv select 1)
-BRANCH_MISSES=$(  grep ',branch-misses,' <$RESULTS | xsv select 1)
-
-echo "commit,timestamp,task clock,units,cpu utilization,instructions,cycles,branches,branch misses"
-echo "$COMMIT,$(date -Iseconds),$TASK_CLOCK,$CLOCK_UNITS,$CPU_UTILIZATION,$INSTRUCTIONS,$CYCLES,$BRANCHES,$BRANCH_MISSES"
+# We're using `time` to measure the max RSS, but note that it includes the
+# RSS of the `perf` program too.
+/usr/bin/time -f'%M,KB,max-rss,,,,' -- \
+    perf stat --field-separator="," -- \
+    $@ 2>&1 >&- |
+    awk -F, '
+BEGIN { print "task clock,units,cpu utilization,context switches,cpu migrations,page faults,cycles,instructions,branches,branch misses,max rss" }
+/,task-clock,/ { task_clock = $1; clock_units = $2; cpu_utilization = $6 }
+/,context-switches,/ { context_switches = $1 }
+/,cpu-migrations,/ { cpu_migrations = $1 }
+/,page-faults,/ { page_faults = $1 }
+/,cycles,/ { cycles = $1 }
+/,instructions,/ { instructions = $1 }
+/,branches,/ { branches = $1 }
+/,branch-misses,/ { branch_misses = $1 }
+/,max-rss,/ { max_rss = $1 }
+END { print task_clock","units","cpu_utilization","context_switches","cpu_migrations","page_faults","cycles","instructions","branches","branch_misses","max_rss }
+'
