0af8473a02b0bbd63013d59040b9189f87e7a345
Alex Sayers <alex@asayers.com>
2020-01-17 23:36:00 +0900

    Inline some functions with a single call site

--- src/analyze.rs
+++ src/analyze.rs
@@ -151,18 +151,14 @@ impl Measurements {
         &self.msmts[bench.0 * self.stride..(bench.0 + 1) * self.stride]
     }
 
-    fn bench_stats_mut(&mut self, bench: Bench) -> &mut [confidence::StatsBuilder] {
+    pub fn update(&mut self, bench: Bench, new_measurements: impl Iterator<Item = f64>) {
         let start = self.stride * bench.0;
         let end = self.stride * (bench.0 + 1);
         if self.msmts.len() < end {
             self.msmts
                 .resize_with(end, confidence::StatsBuilder::default);
         }
-        &mut self.msmts[start..end]
-    }
-
-    pub fn update(&mut self, bench: Bench, new_measurements: impl Iterator<Item = f64>) {
-        for (stats, msmt) in self.bench_stats_mut(bench).iter_mut().zip(new_measurements) {
+        for (stats, msmt) in self.msmts[start..end].iter_mut().zip(new_measurements) {
             stats.update(msmt);
         }
     }
@@ -180,23 +176,14 @@ impl Measurements {
 #[derive(Debug, Clone, PartialEq, Copy)]
 pub struct DiffCI(pub confidence::Stats, pub confidence::Stats);
 impl DiffCI {
-    /// The confiendence interval for the difference of the means, given as
+    /// The confidence interval for the difference of the means, given as
     /// a percentage of the first mean.
     pub fn interval(self, sig_level: f64) -> (f64, f64) {
+        let width =
+            confidence::confidence_interval(sig_level, self.0, self.1).unwrap_or(std::f64::NAN);
         let delta = self.1.mean - self.0.mean;
-        let width = self.width(sig_level);
         let left = 100. * (delta - width) / self.0.mean;
         let right = 100. * (delta + width) / self.0.mean;
         (left, right)
-    }
-
-    fn width(self, sig_level: f64) -> f64 {
-        confidence::confidence_interval(sig_level, self.0, self.1).unwrap_or_else(|e| {
-            match e {
-                confidence::Error::NotEnoughData => (), // we expect some of these; ignore
-                e => warn!("Skipping bad stats: {} ({:?} {:?})", e, self.0, self.1),
-            };
-            std::f64::NAN
-        })
     }
 }
