b313f2a42e1ab2c8dd212ddd22105d57a8978a16
Alex Sayers <alex@asayers.com>
2020-01-17 18:48:28 +0900

    Extract time_cmd crate

--- src/sample.rs
+++ src/sample.rs
@@ -3,8 +3,8 @@ use std::collections::{BTreeMap, BTreeSet};
 use std::fmt;
 use std::io::Write;
 use std::process::{Command, Stdio};
-use std::time::*;
 use structopt::*;
+use time_cmd::*;
 
 #[derive(StructOpt)]
 pub struct Options {
@@ -133,60 +133,4 @@ fn run_bench(bench: &Benchmark) -> Result<BTreeMap<String, f64>> {
                 .with_context(|| String::from_utf8_lossy(&out.stderr).into_owned())
         }
     }
-}
-
-#[derive(Clone, Copy, PartialEq, Debug)]
-struct Timings {
-    wall_time: Duration,
-    user_time: f64,
-    sys_time: f64,
-}
-
-/// The user must ensure that no other child processes are running.
-fn time_cmd(cmd: Command) -> Result<Timings> {
-    #[cfg(unix)]
-    let ret = time_cmd_posix(cmd)?;
-    #[cfg(not(unix))]
-    let ret = time_cmd_fallback(cmd)?;
-    Ok(ret)
-}
-
-#[allow(unused)]
-fn time_cmd_fallback(mut cmd: Command) -> Result<Timings> {
-    let ts = Instant::now();
-    cmd.spawn()?.wait()?;
-    let d = ts.elapsed();
-    Ok(Timings {
-        wall_time: d,
-        user_time: 0.,
-        sys_time: 0.,
-    })
-}
-
-#[cfg(unix)]
-fn time_cmd_posix(mut cmd: Command) -> Result<Timings> {
-    // times(2) and sysconf(2) are both POSIX
-    let mut tms_before = libc::tms {
-        tms_utime: 0,
-        tms_stime: 0,
-        tms_cutime: 0,
-        tms_cstime: 0,
-    };
-    let mut tms_after = tms_before;
-
-    unsafe { libc::times(&mut tms_before as *mut libc::tms) };
-    let ts = Instant::now();
-    cmd.spawn()?.wait()?;
-    let d = ts.elapsed();
-    unsafe { libc::times(&mut tms_after as *mut libc::tms) };
-
-    let ticks_per_sec = unsafe { libc::sysconf(libc::_SC_CLK_TCK) } as f64;
-    let utime = (tms_after.tms_cutime - tms_before.tms_cutime) as f64 / ticks_per_sec;
-    let stime = (tms_after.tms_cstime - tms_before.tms_cstime) as f64 / ticks_per_sec;
-
-    Ok(Timings {
-        wall_time: d,
-        user_time: utime,
-        sys_time: stime,
-    })
 }
---
+++ time_cmd/src/lib.rs
@@ -0,0 +1,59 @@
+use std::io::Result;
+use std::process::Command;
+use std::time::{Duration, Instant};
+
+#[derive(Clone, Copy, PartialEq, Debug)]
+pub struct Timings {
+    pub wall_time: Duration,
+    pub user_time: f64,
+    pub sys_time: f64,
+}
+
+/// The user must ensure that no other child processes are running.
+pub fn time_cmd(cmd: Command) -> Result<Timings> {
+    #[cfg(unix)]
+    let ret = time_cmd_posix(cmd)?;
+    #[cfg(not(unix))]
+    let ret = time_cmd_fallback(cmd)?;
+    Ok(ret)
+}
+
+#[allow(unused)]
+fn time_cmd_fallback(mut cmd: Command) -> Result<Timings> {
+    let ts = Instant::now();
+    cmd.spawn()?.wait()?;
+    let d = ts.elapsed();
+    Ok(Timings {
+        wall_time: d,
+        user_time: 0.,
+        sys_time: 0.,
+    })
+}
+
+#[cfg(unix)]
+fn time_cmd_posix(mut cmd: Command) -> Result<Timings> {
+    // times(2) and sysconf(2) are both POSIX
+    let mut tms_before = libc::tms {
+        tms_utime: 0,
+        tms_stime: 0,
+        tms_cutime: 0,
+        tms_cstime: 0,
+    };
+    let mut tms_after = tms_before;
+
+    unsafe { libc::times(&mut tms_before as *mut libc::tms) };
+    let ts = Instant::now();
+    cmd.spawn()?.wait()?;
+    let d = ts.elapsed();
+    unsafe { libc::times(&mut tms_after as *mut libc::tms) };
+
+    let ticks_per_sec = unsafe { libc::sysconf(libc::_SC_CLK_TCK) } as f64;
+    let utime = (tms_after.tms_cutime - tms_before.tms_cutime) as f64 / ticks_per_sec;
+    let stime = (tms_after.tms_cstime - tms_before.tms_cstime) as f64 / ticks_per_sec;
+
+    Ok(Timings {
+        wall_time: d,
+        user_time: utime,
+        sys_time: stime,
+    })
+}
