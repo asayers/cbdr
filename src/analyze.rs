3bb30d65619dbe68d8da62e9a8d62f109bf20c90
Alex Sayers <alex@asayers.com>
2024-10-02 18:45:26 +0900

    analyze: Show the base benchmark first

--- src/analyze.rs
+++ src/analyze.rs
@@ -40,7 +40,7 @@ impl Options {
             Box::new(
                 self.labels_in_order()
                     .filter(move |x| *x != base)
-                    .map(move |x| (x, base)),
+                    .map(move |x| (base, x)),
             )
         } else {
             Box::new(self.labels_in_order().zip(self.labels_in_order().skip(1)))
