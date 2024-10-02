7139d82f4f3b4acf58efdeab941a7b84af7ce171
Alex Sayers <alex@asayers.com>
2024-10-03 08:50:47 +0900

    Rename default metrics for clarity

--- src/sample.rs
+++ src/sample.rs
@@ -182,14 +182,14 @@ fn run_bench(bench: &Benchmark) -> Result<BTreeMap<String, f64>> {
                 .arg(x)
                 .stdout(Stdio::null())
                 .stderr(Stdio::null());
-            let mut ret = BTreeMap::default();
             let (timings, status) = time_cmd(cmd)?;
             if !status.success() {
                 bail!("{}: Benchmark exited non-zero ({})", bench, x);
             }
-            ret.insert("wall_time".into(), timings.wall_time.as_secs_f64());
-            ret.insert("user_time".into(), timings.user_time);
-            ret.insert("sys_time".into(), timings.sys_time);
+            let mut ret = BTreeMap::default();
+            ret.insert("wall_clock_secs".into(), timings.wall_time.as_secs_f64());
+            ret.insert("user_cpu_secs".into(), timings.user_time);
+            ret.insert("kernel_cpu_secs".into(), timings.sys_time);
             Ok(ret)
         }
         BenchRunner::Script(script, args) => {
